45209d8030a9028cfb4bc6c99e5f41b85756ead3
Alberto Madueno <gato-negro@Albertos-MBP.attlocal.net>
2018-11-24 22:27:23 -0800

    update webpack for v4 compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const path = require('path');
 const loader = require('babel-loader');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
 const BUILD_DIR = path.resolve(__dirname, 'client/dist');
 const APP_DIR = path.resolve(__dirname, 'client/src/app/index.js');
@@ -23,14 +25,26 @@ const config = {
         use: {
           loader: "babel-loader"
         }
-      },
+      },      
       {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader']
-      },
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          // 'postcss-loader',
+          'sass-loader',
+        ],
+      }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    })
+  ],
   watch: true
 };
 
