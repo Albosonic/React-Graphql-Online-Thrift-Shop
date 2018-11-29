ccf862829c49d5e0d88e5800d24250642b673f57
Alberto Madueno <gato-negro@Albertos-MBP.attlocal.net>
2018-11-28 18:26:59 -0800

    place feed on dashboard

--- client/src/app/dashboard/dashboard.js
+++ client/src/app/dashboard/dashboard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Activities from "../activities/activities";
 import items from '../mock-items-data';
+import Feed from '../feed/feed';
 import './dashboard.scss';
 
 class Dashboard extends React.Component {
   render () {
     return (
+
       <div className="dashboard-container">        
         <Activities storeItems={ items }/>
+        <div className="feed-container">
+          <h1>Feed</h1>
+          <Feed/>
+        </div>
       </div>
     )
   }
