4bc299c8be7aa51cbdf29c4fe9976027f140f0c2
Alberto Madueno <albertomadueno@Albertos-Air.attlocal.net>
2018-12-11 18:37:21 -0800

    react elements basic setup

--- client/src/app/account-settings/account-settings.js
+++ client/src/app/account-settings/account-settings.js
@@ -1,12 +1,17 @@
-import React from 'react';
 import './account-settings.scss';
+import React from 'react';
+import { StripeProvider, Elements } from 'react-stripe-elements';
+
+import InjectedCheckoutForm from '../checkout/checkout';
 
 class AccountSettings extends React.Component {
   render () {
     return (
-      <div className="account-settings-container">
-        <h1>Account Settings</h1>
-      </div>
+      <StripeProvider apiKey="pk_test_baS4eaKTzJ3xiXpz2yPtRwjO">
+        <Elements>
+          <InjectedCheckoutForm />
+        </Elements>
+      </StripeProvider>
     )
   }
 }
